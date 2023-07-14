ceda316819d7e2aa944447a1ef7f2419038374b8
Pavlo Safonov <psp@sitecore.net>
2023-07-14 20:01:03 +0300

    тести

--- tests/ddt.test.ts
+++ tests/ddt.test.ts
@@ -2,13 +2,41 @@ import { test } from "@playwright/test";
 import { MenuPage } from "../project/pages/MenuPage";
 import { readCsvFile } from "../helpers/csvHelper";
 
-const data = readCsvFile("data.csv");
+test.describe("Example of Parametrized test with object data import", () => {
+  const data = [
+    {
+      testID: 1,
+      drinks: ["Espresso", "Espresso"],
+    },
+    {
+      testID: 2,
+      drinks: "Mocha",
+    },
+    {
+      testID: 3,
+      drinks: ["Espresso", "Espresso", "Cafe Latte"],
+    },
+  ];
 
-for (const { testID, drinks } of data) {
-  test(`${testID} - DDT examples - add drinks to cart`, async ({ page }) => {
-    const menuPage = new MenuPage(page);
+  for (const { testID, drinks } of data) {
+    test(`${testID} - DDT examples - add drinks to cart`, async ({ page }) => {
+      const menuPage = new MenuPage(page);
 
-    await menuPage.navigateTo();
-    await menuPage.addDrinkToCart(drinks);
-  });
-}
+      await menuPage.navigateTo();
+      await menuPage.addDrinkToCart(drinks);
+    });
+  }
+});
+
+test.describe("Example of DDT test with CSV data import", () => {
+  const data = readCsvFile("data.csv");
+
+  for (const { testID, drinks } of data) {
+    test(`${testID} - DDT examples - add drinks to cart`, async ({ page }) => {
+      const menuPage = new MenuPage(page);
+
+      await menuPage.navigateTo();
+      await menuPage.addDrinkToCart(drinks);
+    });
+  }
+});
